75b8b59b336358094769811d0a68ded677d86e3b
GostWave <gostwave@mail.ru>
2025-04-20 15:41:33 +0300

    Import images

--- Import_data.py
+++ Import_data.py
@@ -1,22 +1,39 @@
+import os.path
 from sys import audit
 
 
 
 
-def getquestions():
+def getquestions_yes_no():
     import pandas as pd
-    file_path = "Data2.csv"
+    file_path = "Data.csv"
 
     # Считывание базы вопросов
 
     df = pd.read_csv(file_path)
+    df["Номер вопроса"] = df["Номер вопроса"].ffill()
+
     limited_df = df.iloc[:20]
 
     #Заполнение вопросами с ответами да или нет
 
     questions_yesno = tuple(
-        {"Вопрос": row["Вопрос"], "Ответ": row["Ответ"]}
+        {"Номер вопроса": int(row["Номер вопроса"]), "Вопрос": row["Вопрос"], "Ответ": row["Ответ"]}
         for _, row in limited_df.iterrows()
         if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"])
     )
     return questions_yesno
+
+def getquestions_with_img():
+    import pandas as pd
+    file_path = "Data.csv"
+    df = pd.read_csv(file_path)
+    df["Номер вопроса"] = df["Номер вопроса"].ffill()
+
+    limited_df = df.iloc[20:22]
+    questions_with_img= tuple(
+        {"Номер вопроса": int(row["Номер вопроса"]), "Вопрос": row["Вопрос"], "Картинка": row["Картинка"], "Ответ": row["Ответ"]}
+        for _, row in limited_df.iterrows()
+        if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"]) and os.path.exists(row["Картинка"])
+    )
+    return questions_with_img
--- QuestionManager.py
+++ QuestionManager.py
@@ -1,5 +1,11 @@
 import Import_data
+from PIL import Image
 
 
+for i in Import_data.getquestions_yes_no():
+    print(i)
 
-print(Import_data.getquestions())
+for i in Import_data.getquestions_with_img():
+    image_path= str(i["Картинка"])
+    img = Image.open(image_path)
+    img.show()
