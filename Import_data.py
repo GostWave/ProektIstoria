364650851c219e42dcce5ed011caf6ee9cdc07ab
GostWave <gostwave@mail.ru>
2025-04-28 00:25:30 +0300

    Add function getquestions_with_choice

--- Import_data.py
+++ Import_data.py
@@ -14,20 +14,20 @@ def getquestions_yes_no():
     df["Номер вопроса"] = df["Номер вопроса"].ffill()
     df["Тема"] = df["Тема"].ffill()
 
-    limited_df = df.iloc[:20]
+    # limited_df = df.iloc[]
 
     #Заполнение вопросами с ответами да или нет
 
     questions_yesno = tuple(
         {"Тема": row["Тема"],"Номер вопроса": int(row["Номер вопроса"]), "Вопрос": row["Вопрос"], "Ответ": row["Ответ"]}
-        for _, row in limited_df.iterrows()
-        if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"])
+        for _, row in df.iterrows()
+        if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"]) and (int(row["Номер вопроса"])==1 or int(row["Номер вопроса"])==2)
     )
     return questions_yesno
 
 def getquestions_with_img():
     import pandas as pd
-    file_path = "Data.csv"
+    file_path = "Data2.csv"
     df = pd.read_csv(file_path)
     df["Номер вопроса"] = df["Номер вопроса"].ffill()
     df["Тема"] = df["Тема"].ffill()
@@ -35,6 +35,26 @@ def getquestions_with_img():
     questions_with_img= tuple(
         {"Тема": row["Тема"],"Номер вопроса": int(row["Номер вопроса"]), "Вопрос": row["Вопрос"], "Картинка": row["Картинка"], "Ответ": row["Ответ"]}
         for _, row in limited_df.iterrows()
-        if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"]) and os.path.exists(row["Картинка"])
+        if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"]) and os.path.exists(row["Картинка"]) and (int(row["Номер вопроса"])==3 or int(row["Номер вопроса"])==4)
     )
     return questions_with_img
+
+def getquestions_with_choice():
+    import pandas as pd
+    file_path = "Data.csv"
+
+
+    df = pd.read_csv(file_path)
+    df["Номер вопроса"] = df["Номер вопроса"].ffill()
+    df["Тема"] = df["Тема"].ffill()
+
+    # limited_df = df.iloc[]
+
+
+
+    questions_with_choice = tuple(
+        {"Тема": row["Тема"], "Номер вопроса": int(row["Номер вопроса"]), "Вопрос": row["Вопрос"],"Ответ": str(row["Ответ"]).split("/"), "Неверные ответы" : str(row["Неверные ответы"]).split(", ")}
+        for _, row in df.iterrows()
+        if pd.notna(row["Вопрос"]) and pd.notna(row["Ответ"]) and pd.notna(row["Неверные ответы"]) and (int(row["Номер вопроса"]) == 3 or int(row["Номер вопроса"]) == 4)
+    )
+    return questions_with_choice
--- QuestionManager.py
+++ QuestionManager.py
@@ -2,11 +2,14 @@ import Import_data
 from PIL import Image
 
 
-for i in Import_data.getquestions_yes_no():
+# for i in Import_data.getquestions_yes_no():
+#     print(i)
+
+for i in Import_data.getquestions_with_choice():
     print(i)
 
-for i in Import_data.getquestions_with_img():
-    image_path= str(i["Картинка"])
-    img = Image.open(image_path)
-    print(i)
-    img.show()
+# for i in Import_data.getquestions_with_img():
+#     image_path= str(i["Картинка"])
+#     img = Image.open(image_path)
+#     print(i)
+#     img.show()
